1e3d92b849d65d8c165370526efc5cfe54d6827a
PuPuppru <zhengruru123@gmail.com>
2020-04-06 02:00:18 +0800

    Revert "vibration control v7"
    This reverts commit f7843b61ee6242063c2bc3fa0643cd5e3fa8e43c.

--- drivers/leds/leds-qpnp-haptics.c
+++ drivers/leds/leds-qpnp-haptics.c
@@ -1926,9 +1926,7 @@ static ssize_t qpnp_haptics_show_vmax_mv_strong(struct device *dev,
 	if (rc < 0) {
 		chip->vmax_mv_strong = old_vmax_mv;
 		return rc;
-    }	
-    return count;
-}
+}		
 
 static ssize_t qpnp_haptics_show_vmax_override(struct device *dev,
 		struct device_attribute *attr, char *buf)
